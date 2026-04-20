3412a9b5abd87f7526ca3580687cac059798b0cb
Nurbu <lamanurbu2@gmail.com>
2026-04-20 10:34:54 -0400

    Finished getting all the quotes.

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
 public class FamousQuotes {
+    public static void main(String[] args) {
+        String[] quotes = new String[10];
+
+        quotes[0] = "If you can change you mind, you can change your life.";
+        quotes[1] = "You define your own life. Don't let other people write your script.";
+        quotes[2] = "Be kind whenever possible. It is always possible.";
+        quotes[3] = "You can have it all. You just can't have it all at once.";
+        quotes[4] = "Don't feel quilty for doing what's best for you.";
+        quotes[5] = "Some people want it to happen, some wish it would happen, others make it happen";
+        quotes[6] = "A winer is a dreamer who never gives up.";
+        quotes[7] = "Never bend your head. Always hold it high. Look the world straight in the eye.";
+        quotes[8] = "Have no fear of perfection, you'll never reach it.";
+        quotes[9] = "Live as if you were to die tomorrow. Learn as if you were to live forever.";
+        
+    }
+
 }
